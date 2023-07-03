14d200acf2bcc8ae3ac7e9e77c672e851db6898a
manasa.ch <manasa.c@marketsimplified.com>
2023-07-03 18:01:01 +0530

    added !!

---
+++ src/main/kotlin/operations.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var firstName :String = "Manasa"
+    var lastName = "chebolu"
+    println(firstName + " " + lastName)
+    println("$firstName $lastName")
+    println(firstName.plus("ch"))
+    println(firstName.compareTo(lastName))
+    println(firstName.indexOf("Man"))
+    println(firstName.indexOf("ee")) // return -1
+    println(firstName[2])
+    var x= 5
+    val y=6
+    println(x.toString())
+    println(y+6)
+    println(x==5)
+    println(x==y)
+
+}
